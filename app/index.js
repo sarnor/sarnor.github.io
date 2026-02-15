54e45a53559616b574d6b94e62f0a30f1d8100d3
sarnor <sarnor@mail.ru>
2026-02-15 16:11:47 +0400

    15-02-2026 16:11

--- app/index.js
+++ app/index.js
@@ -1,2 +1,6 @@
-(()=>{"use strict";let e;setTimeout(()=>{e=document.querySelector(".loader"),console.log("🚀 ~ loader:",e),e.classList.add("remove")},5e3)})();
-//# sourceMappingURL=index.js.map
+let e;
+setTimeout(() => {
+    e = document.querySelector(".loader");
+    console.log("🚀 ~ loader:", e);
+    e.classList.add("remove")
+}, 2000)
